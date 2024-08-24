cc85128aad0826c6e72adb23c20e8a013ba58002
TeamInfinity2 <Fabihanaseem2@gmail.com>
2024-08-25 02:02:09 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,56 @@
+// QUESTION NO 1
+// alert ( "Hi visitor... thanks for visiting us!" );
+// QUESTION NO 2
+// alert("Error! please enter a valid password")
+// QUESTION NO 3
+// alert("Welcome to JS Land... \nHappy Coding...!! ")
+// question no 4
+// alert("Welcome to JS Land...")
+// alert("\nHappy Coding...!! ")
+// question5
+// alert("Hellow... i can run JS through my web browser's web console")
+// chapter 2
+// question no 1
+// var username;
+// var username = "YourName";
+// question2
+// var myName = "Your Full Name";
+// question3
+// var message;
+// q 4
+// var message = "hellow world";
+// q3c
+// var message = "Hello, this is your message!";
+// alert(message);
+// q4
+// var studentName = "Jane";
+// var age = "20 years old";
+// var course = "Web Development";
+// alert("Student Name: ");
+// alert("Age: " + age);
+// alert("Course: " + course);
+// q5
+// var message = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(message);
+// q6
+// var email = "example@example.com";
+// alert("My email address is " + email);
+// q7
+// var bookname = "book"
+// alert("A smarter way to learn JS")
+// Q8
+// var heading = "hellow"
+// document.write("yaa i can write html through js")
+// q9
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+// CHAPTER=3
+// Q1
+// var age = 19
+// alert("i am 19 years old")
+// Q2
+// var birthyear = "2005"
+// document.write("its my birth year")
+// Q3
+// document.write("John Doe ordered 5 T-shirt(s) on XYZ Clothing store")
+alert("the end")
